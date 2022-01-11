d8f5dfa58342ac786b808902569c0d0aa55dd63b
DayceeLau <97490603+DayceeLau@users.noreply.github.com>
2022-01-11 14:54:41 +0800

    Add files via upload

---
+++ 2101601_Lab01_Wk01/Lab2a.java
@@ -0,0 +1,9 @@
+public class Lab2a 
+{
+    public static void main(String[] args) 
+    {
+        //Reference to Example1
+        System.out.println("Hello, I am Daycee!");
+        System.out.println();
+    }    
+}
